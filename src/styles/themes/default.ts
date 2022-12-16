9630c1ed40ccd034cc134d7c53f918afd118a0c9
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-12-16 16:18:26 -0300

    feat: create defacult style

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,74 @@
+export const defaultTheme = {
+  colors: {
+    primary: "#000000",
+    secondary: "#ffffff",
+    background: "#ffffff",
+    text: "#000000",
+    border: "#000000",
+    notification: "#000000",
+  },
+  spacing: {
+    xs: 4,
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 40,
+  },
+  borderRadius: {
+    s: 4,
+    m: 10,
+    l: 25,
+    round: 50,
+  },
+  textVariants: {
+    hero: {
+      fontSize: 70,
+      lineHeight: 80,
+      fontWeight: "bold",
+      color: "background",
+      textAlign: "center",
+    },
+    title1: {
+      fontSize: 28,
+      lineHeight: 30,
+      fontWeight: "bold",
+      color: "text",
+    },
+    title2: {
+      fontSize: 24,
+      lineHeight: 26,
+      fontWeight: "bold",
+      color: "text",
+    },
+    title3: {
+      fontSize: 20,
+      lineHeight: 22,
+      fontWeight: "bold",
+      color: "text",
+    },
+    body: {
+      fontSize: 16,
+      lineHeight: 26,
+      fontWeight: "normal",
+      color: "text",
+    },
+    button: {
+      fontSize: 15,
+      lineHeight: 20,
+      fontWeight: "500",
+      color: "text",
+    },
+    caption: {
+      fontSize: 13,
+      lineHeight: 16,
+      fontWeight: "normal",
+      color: "text",
+    },
+    overline: {
+      fontSize: 10,
+      lineHeight: 15,
+      fontWeight: "500",
+      color: "text",
+    },
+  },
+};
