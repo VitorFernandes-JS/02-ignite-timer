08334217c2a660c61decc90fa4cf2d9791ad9c77
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-12-19 13:25:38 -0300

    fix: routes is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World!</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
